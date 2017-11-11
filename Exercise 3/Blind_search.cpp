03f75d593ba8e729e594b7dafca00ad6d22da204
rafu96 <rafaroco96@gmail.com>
2017-11-11 18:50:18 +0100

    Updating progress

--- Exercise 3/Blind_search.cpp
+++ Exercise 3/Blind_search.cpp
@@ -7,61 +7,6 @@
 
 using namespace std;
 
-// Enumeration representing the orientation, needed to define a path
-enum Orientation { North, East, South, West };
-
-//Read the environment called "nameFile" and saves the dimensions of it in "rows" and "columns"
-void ReadEnvironment( char ** &environment, const char* nameFile, int &rows, int &columns ){
-
-    FILE* file = fopen(nameFile,"r");
-
-    rows=0;
-    columns=0;
-    bool countColumns = true;
-    char c = fgetc(file);
-
-    while( c != EOF ){
-      if( c == '\n' ){
-        rows++;
-        countColumns = false;
-      }
-      if( countColumns ){
-        columns++;
-      }
-      c = fgetc(file);
-    }
-
-    rewind(file);
-    environment = new char*[rows];
-    environment[0] = new char[columns];
-    int currentRow = 0;
-    int currentColumn = 0;
-    c = fgetc(file);
-
-    while( c != EOF ){
-      if( c == '\n' ){
-        currentRow++;
-        environment[currentRow] = new char[columns];
-        currentColumn = 0;
-      }
-      else{
-        environment[currentRow][currentColumn] = c;
-        currentColumn++;
-      }
-      c = fgetc(file);
-    }
-}
-
-// Prints the environment in ASCII code
-void PrintEnvironment( char** environment, const int rows, const int cols ){
-
-  for(int i=0; i<=rows; i++){
-    for(int j=0; j<=cols; j++){
-      cout << environment[i][j];
-    }
-    cout << endl;
-  }
-}
 
 
 // Check if a position is suitable for going thru it
--- Exercise 3/Blind_search.h
+++ Exercise 3/Blind_search.h
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+
+// Enumeration representing the orientation, needed to define a path
+enum Orientation { North, East, South, West };
+
+// Class representing and environment
 class Environment{
   private:
     char** matrix;
@@ -11,6 +16,7 @@ class Environment{
     pair<unsigned int,unsigned int> start,goal;
 
   public:
+    // Constructor for an empty environment with dimensions r and c
     Environment( const unsigned int r, const unsigned int c){
       rows = r;
       columns = c;
@@ -20,22 +26,75 @@ class Environment{
         matrix[i] = new char[cols];
     }
 
+    // Constructor for an environment from a file
     Environment( const char* nameFile ){
-      this.ReadEnvironment
+      this.ReadFile(nameFile);
     }
 
+    // Destructor
     ~Environment(){
       for (int i = 0; i < rows; ++i)
         delete [] matrix[i];
       delete [] matrix;
     }
 
+    // Load an environment from a file
+    bool ReadFile( const char* nameFile ){
+      FILE* file = fopen(nameFile,"r");
+      rows = columns = 0;
+      bool countColumns = true;
+      char c = fgetc(file);
+
+      while( c != EOF ){
+        if( c == '\n' ){
+          rows++;
+          countColumns = false;
+        }
+        if( countColumns ){
+          columns++;
+        }
+        c = fgetc(file);
+      }
+
+      rewind(file);
+      environment = new char*[rows];
+      environment[0] = new char[columns];
+      int currentRow = 0;
+      int currentColumn = 0;
+      c = fgetc(file);
+
+      while( c != EOF ){
+        if( c == '\n' ){
+          currentRow++;
+          environment[currentRow] = new char[columns];
+          currentColumn = 0;
+        }
+        else{
+          environment[currentRow][currentColumn] = c;
+          currentColumn++;
+        }
+        c = fgetc(file);
+      }
+    }
+
+    // Start position getter
     inline pair<unsigned int,unsigned int> getStartPosition(){
       return start;
     }
 
+    // Goal position getter
     inline pair<unsigned int,unsigned int> getGoalPosition(){
       return goal;
+    }
+
+    // Prints the environment in ASCII code
+    void Print( char** environment, const int rows, const int cols ){
+      for(int i=0; i<=rows; i++){
+        for(int j=0; j<=cols; j++){
+          cout << environment[i][j];
+        }
+        cout << endl;
+      }
     }
 
 
