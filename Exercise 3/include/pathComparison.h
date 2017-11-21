bea45aeb6fe908f8ad360cccb3ac68f33b8c7e2f
MigueRobles <miguerobles@correo.ugr.es>
2017-11-21 09:48:21 +0100

    Modularizando...(again)

--- Exercise 3/include/pathComparison.h
+++ Exercise 3/include/pathComparison.h
@@ -1,6 +1,5 @@
 #include <utility>          // std::pair
 #include "environment.h"
-#include "path.h"
 
 
 // Class to compare 2 Paths
@@ -11,10 +10,10 @@ class PathComparison{
     bool reverse;
   public:
     PathComparison( const pair<unsigned int,unsigned int> &g, const Environment* env,
-                    const bool& revparam=false):reverse(revparam),goal(g),environment(env);
+                    const bool& revparam=false);
 
     PathComparison(const unsigned int &x, const unsigned int &y, const Environment* env,
-                    const bool& revparam=false):reverse(revparam),environment(env);
+                    const bool& revparam=false);
 
     bool operator() (const Path &p, const Path &q) const;
 };
