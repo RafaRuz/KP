f2fbe30e9895162be586b0ce445d596b7fa8a980
rafu96 <rafaroco96@gmail.com>
2017-12-06 03:02:59 +0100

    Arc consistency algorithm done

--- Exercise 7/arc_consistency.cpp
+++ Exercise 7/arc_consistency.cpp
@@ -2,18 +2,23 @@
 #include <vector>
 #include <map>
 #include <list>
+#include <queue>
+#include <set>
 
 using namespace std;
 
-template <class T> bool lt( const T &t1, const T &t2 ){ return(t1<t2); }
-template <class T> bool let( const T &t1, const T &t2 ){ return(t1<=t2); }
-template <class T> bool gt( const T &t1, const T &t2 ){ return(t1>t2); }
-template <class T> bool get( const T &t1, const T &t2 ){ return(t1>=t2); }
-template <class T> bool eq( const T &t1, const T &t2 ){ return(t1==t2); }
-template <class T> bool neq( const T &t1, const T &t2 ){ return(t1!=t2); }
 
 
-template <class T>
+
+#define row0 0
+#define row1 1
+#define row2 2
+#define col0 3
+#define col1 4
+#define col2 5
+
+
+/*
 struct Constraint{
     char X,Y;
     bool (*comp)(const T &v1, const T &v2);
@@ -24,44 +29,116 @@ struct Constraint{
       return comp(v1,v2);
     }
 };
-
-/*
-
 */
 
-template <class T>
-void GraphArcConsistency( map<char,T> &variables, map<char,Constraint> &constraints ){
+void GraphArcConsistency( vector< vector<string> > &variables ){
+  list<int> to_do;
 
+  to_do.push_back(row0);
+  to_do.push_back(row1);
+  to_do.push_back(row2);
+  to_do.push_back(col0);
+  to_do.push_back(col1);
+  to_do.push_back(col2);
+
+  while( !to_do.empty() ){
+    int actualVariable = to_do.front();
+    to_do.pop_front();
+
+    bool deleteWord = true;
+    bool deleteWordFinal = false;
+    bool insertedDependencies = false;
+
+    if( actualVariable < 3 ){
+      for( std::vector<string>::iterator it = variables[actualVariable].begin() ; it != variables[actualVariable].end() ; ){
+        deleteWordFinal = false;
+        for (size_t i = col0; i <= col2; i++) {    // We check the 3 columns
+          deleteWord = true;
+          for( vector<string>::iterator it2 = variables[i].begin() ; it2 != variables[i].end() ; it2++ ){
+            if( (*it)[i-3] == (*it2)[actualVariable] ){
+              deleteWord = false;
+            }
+          }
+          if( deleteWord )
+            deleteWordFinal = true;
+        }
+        if( deleteWordFinal ){
+          it = variables[actualVariable].erase(it);
+          if( !insertedDependencies ){
+            to_do.push_back(col0);
+            to_do.push_back(col1);
+            to_do.push_back(col2);
+            to_do.sort();
+            to_do.unique();
+            insertedDependencies = true;
+          }
+        }
+        else it++;
+      }
+    }
+    else{
+      for( vector<string>::iterator it = variables[actualVariable].begin() ; it != variables[actualVariable].end() ; ){
+        deleteWordFinal = false;
+        for (size_t i = row0; i <= row2; i++) {    // We check the 3 rows
+          deleteWord = true;
+          for( vector<string>::iterator it2 = variables[i].begin() ; it2 != variables[i].end() ; it2++ ){
+            if( (*it)[i] == (*it2)[actualVariable-3] ){
+              deleteWord = false;
+            }
+          }
+          if( deleteWord )
+            deleteWordFinal = true;
+        }
+        if( deleteWordFinal ){
+          it = variables[actualVariable].erase(it);
+          if( !insertedDependencies ){
+            to_do.push_back(row0);
+            to_do.push_back(row1);
+            to_do.push_back(row2);
+            to_do.sort();
+            to_do.unique();
+            insertedDependencies = true;
+          }
+        }
+        else it++;
+      }
+
+
+    }
+
+  }
 }
 
 int main(int argc, char const *argv[]) {
-  if( argc != 2 ){
-    cout << "Error." << endl;
-    return 1;
+
+  vector<string> words;
+  words.push_back("add");  words.push_back("ado");  words.push_back("age");  words.push_back("ago");
+  words.push_back("aid");  words.push_back("air");  words.push_back("ail");  words.push_back("aim");
+  words.push_back("and");  words.push_back("any");  words.push_back("ape");  words.push_back("apt");
+  words.push_back("arc");  words.push_back("are");  words.push_back("ark");  words.push_back("arm");
+  words.push_back("art");  words.push_back("ash");  words.push_back("ask");  words.push_back("auk");
+  words.push_back("awe");  words.push_back("awl");  words.push_back("aye");  words.push_back("bad");
+  words.push_back("bag");  words.push_back("ban");  words.push_back("bat");  words.push_back("bee");
+  words.push_back("boa");  words.push_back("ear");  words.push_back("eel");  words.push_back("eft");
+  words.push_back("far");  words.push_back("fat");  words.push_back("fit");  words.push_back("lee");
+  words.push_back("oaf");  words.push_back("rat");  words.push_back("tar");  words.push_back("tie");
+
+
+  vector< vector<string> > variables;
+
+  for (size_t i = 0; i < 6; i++) {
+    variables.push_back(words);
   }
 
-  vector<string> list;
-  list.push_back("add");  list.push_back("ado");  list.push_back("age");  list.push_back("ago");
-  list.push_back("aid");  list.push_back("air");  list.push_back("ail");  list.push_back("aim");
-  list.push_back("and");  list.push_back("any");  list.push_back("ape");  list.push_back("apt");
-  list.push_back("arc");  list.push_back("are");  list.push_back("ark");  list.push_back("arm");
-  list.push_back("art");  list.push_back("ash");  list.push_back("ask");  list.push_back("auk");
-  list.push_back("awe");  list.push_back("awl");  list.push_back("aye");  list.push_back("bad");
-  list.push_back("bag");  list.push_back("ban");  list.push_back("bat");  list.push_back("bee");
-  list.push_back("boa");  list.push_back("ear");  list.push_back("eel");  list.push_back("eft");
-  list.push_back("far");  list.push_back("fat");  list.push_back("fit");  list.push_back("lee");
-  list.push_back("oaf");  list.push_back("rat");  list.push_back("tar");  list.push_back("tie");
+  GraphArcConsistency(variables);
 
-
-
-
-
-
-
-
-
-
-
+  for (size_t i = 0; i < variables.size(); i++) {
+    cout << "Posicion " << i << "-->";
+    for (size_t j = 0; j < variables[i].size(); j++) {
+      cout << variables[i][j] << ", ";
+    }
+    cout << endl;
+  }
 
 
   return 0;
